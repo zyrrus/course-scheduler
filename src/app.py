0ff205a9452446a611fb1c0e758761e48d9d7816
Zeke <zekeabshire@gmail.com>
2021-07-21 21:29:31 -0500

    hid secret key

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from flask import Flask, flash, render_template, request, redirect, url_for, make_response
 from flask_wtf import FlaskForm
@@ -8,14 +9,11 @@ from schedule.manage import Manager, Week
 
 # ===============================================
 # TODO: 
-# - Clean up css and anything else that 
-# could use some help
-# - Fix secret key and remove dev mode
-# - Add flashes for courses added
+# - Add flash for courses added
 # ===============================================
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-secret-key-that-no-one-knows'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 class CourseForm(FlaskForm):
@@ -133,8 +131,5 @@ def set_index(change):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
-''' Generic course info 
-[{"name":"CSC 2362","start":"15:00","end":"16:20","days":["M", "W"]},{"name":"CSC 3200","start":"10:30","end":"11:20","days":["W"]},{"name":"CSC 3380","start":"16:30","end":"17:50","days":["T", "Th"]},{"name":"CSC 4330","start":"15:00","end":"16:20","days":["T", "Th"]},{"name":"CSC 3501","start":"13:30","end":"14:50","days":["T", "Th"]},{"name":"CSC 3730","start":"09:00","end":"10:20","days":["T", "Th"]},{"name":"CSC 4101","start":"12:00","end":"13:20","days":["M", "W"]},{"name":"CSC 4103","start":"13:30","end":"14:50","days":["M", "W"]},{"name":"CSC 4444","start":"15:30","end":"16:50","days":["M", "W"]},{"name":"MATH 2060","start":"12:30","end":"13:20","days":["T", "Th"]},{"name":"MATH 4020","start":"13:30","end":"14:50","days":["T", "Th"]},{"name":"MATH 4031","start":"09:30","end":"10:20","days":["M", "W", "F"]},{"name":"MATH 4065","start":"10:30","end":"11:50","days":["T", "Th"]},{"name":"MATH 4153","start":"15:00","end":"16:20","days":["T", "Th"]},{"name":"MATH 4200","start":"08:30","end":"09:20","days":["M", "W", "F"]},
-'''
